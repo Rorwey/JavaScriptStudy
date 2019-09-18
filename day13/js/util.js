cb8ccfda418cb71586f03b67ae046cfa9743def0
LuoV <rorwey@foxmail.com>
2019-09-18 11:30:06 +0800

    Update util.js

--- day13/js/util.js
+++ day13/js/util.js
@@ -12,4 +12,38 @@ function getStyle(obj, name) {
 	}
 };
 
+/**
+ * 一个可以移动的简单动画的函数
+ * 参数：
+ * - obj: 要执行动画的对象
+ * - attr: 要执行动画的样式，例如left top width height
+ * - target: 执行动画的停止位置
+ * - speed: 移动的速度
+ * - callback: 将会在动画执行完毕以后执行
+ */
+function move(obj, attr, target, speed, callback) {
+	clearInterval(obj.timer);
+	var current = parseInt(getStyle(obj, attr));
+	//如果从小向大移动,则speed为正。否则为负
+	if (current > target) {
+		speed = -speed;
+	}
+	//开启定时器,用来执行动画效果
+	//向执行动画的对象中添加一个timer,用来保存他自己的定时器
+	obj.timer = setInterval(function() {
+			var oldValue = parseInt(getStyle(obj, attr));
+			var newValue = oldValue + speed;
+			if ((speed < 0 && newValue < target) || (speed > 0 && newValue > target)) {
+				newValue = target;
+			}
+			obj.style[attr] = newValue + "px";
+			//当元素移动到target,使其停止动画
 
+			if (newValue == target) {
+				clearInterval(obj.timer);
+				//动画执行完毕,调用回调函数
+				callback && callback();
+			}
+		},
+		30);
+}
